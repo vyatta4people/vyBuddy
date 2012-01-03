e84ce191c71af1b3dc120866063d869de9d3fa18
Vyatta4People <vyatta4people@vyatta4people.org>
2012-01-04 00:33:21 +0200

    Added AutoScroll to display_tasks_tab_panel component.

--- app/components/display_tasks_tab_panel.rb
+++ app/components/display_tasks_tab_panel.rb
@@ -15,12 +15,13 @@ class DisplayTasksTabPanel < Netzke::Basepack::TabPanel
       task_group_item[:deferred_render]   = false
       task_group_item[:items]             = Array.new
       task_group.tasks.where(:is_enabled => true).each do |task|
-        task_item              = Hash.new
-        task_item[:id]         = task.html_id
-        task_item[:name]       = task_item[:id]
-        task_item[:title]      = task.name
-        task_item[:class_name] = "Netzke::Basepack::Panel"
-        task_item[:html]       = task.task_remote_commands.collect{ |trc| "<pre class='display-information'><div id='#{trc.html_display_id}' class='display'></div></pre>" }.join
+        task_item               = Hash.new
+        task_item[:id]          = task.html_id
+        task_item[:name]        = task_item[:id]
+        task_item[:title]       = task.name
+        task_item[:class_name]  = "Netzke::Basepack::Panel"
+        task_item[:auto_scroll] = true
+        task_item[:html]        = task.task_remote_commands.collect{ |trc| "<pre class='display-information'><div id='#{trc.html_display_id}' class='display'></div></pre>" }.join
         task_group_item[:items] << task_item
       end
       task_group_items << task_group_item
