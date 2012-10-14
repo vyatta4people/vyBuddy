dcffe12d867f5b939b60ea47d0956b89429ab6e1
Vyatta4People <vyatta4people@vyatta4people.org>
2012-10-14 17:21:10 +0300

    Updated ruby linking rules.

--- vendor/vybuddy/update/script.d/2012101401_ln_ruby.sh
+++ vendor/vybuddy/update/script.d/2012101401_ln_ruby.sh
@@ -1,2 +1,4 @@
-sudo rm -f /usr/bin/ruby
-sudo ln -s /usr/local/rvm/rubies/ruby-1.9.3-head/bin/ruby /usr/bin/ruby
+if [ "$(ruby -v | fgrep 1.9.3)" ]; then
+  sudo rm -f /usr/bin/ruby
+  sudo ln -s /usr/local/rvm/rubies/ruby-1.9.3-head/bin/ruby /usr/bin/ruby
+fi
