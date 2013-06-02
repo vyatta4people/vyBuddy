6bcd36a2d20b1ffd8c29d2847c46c779c4ea90a1
Vyatta4People <vyatta4people@vyatta4people.org>
2013-06-02 16:17:45 +0300

    Rewrote login_window component to conform Netzke 0.8 conventions.

--- app/components/login_window.rb
+++ app/components/login_window.rb
@@ -1,54 +1,57 @@
 class LoginWindow < Netzke::Basepack::Window
 
-  js_mixin :init_component
-  js_mixin :actions
-
-  action :login, :icon => :tick, :text => "Login"
-
-  def configuration
-    super.merge(
-      :name             => :login_window,
-      :title            => "::Login::",
-      :width            => 400,
-      :y                => 100,
-      :border           => false,
-      :modal            => true,
-      :closable         => false,
-      :resizable        => false,
-      :draggable        => false,
-      :hidden           => false,
-      :items            => [ {
-        :id               => :login_form,
-        :xtype            => :form,
-        :prevent_header   => true,
-        :border           => false,
-        :frame            => true,
-        :buttons          => [ :login.action ],
-        :default_type     => :textfield,
-        :defaults         => { :anchor => "100%", :label_pad => 0, :allow_blank => false },
-        :items            => [ {
-          :name         => :username,
-          :field_label  => "Username"
-        }, {
-          :name         => :password,
-          :field_label  => "Password",
-          :input_type   => :password
-        }, {
-          :name         => :log_application,
-          :value        => super[:log_application],
-          :hidden       => true
-        }, {
-          :name         => :log_event_source,
-          :value        => super[:log_event_source],
-          :hidden       => true
-        } ]
-      } ]
-    )
+  action :login do |a|
+    a.icon = :tick
+    a.text = "Login"
   end
 
-  endpoint :login do |params|
-    success = false
-    user = User.find_by_username(params[:username])
+  def configure(c)
+    super
+    c.name             = :login_window
+    c.title            = "::Login::"
+    c.width            = 400
+    c.y                = 100
+    c.border           = false
+    c.modal            = true
+    c.closable         = false
+    c.resizable        = false
+    c.draggable        = false
+    c.hidden           = false
+    c.items            = [ {
+      :id               => :login_form,
+      :xtype            => :form,
+      :prevent_header   => true,
+      :border           => false,
+      :frame            => true,
+      :buttons          => [ :login ],
+      :default_type     => :textfield,
+      :defaults         => { :anchor => "100%", :label_pad => 0, :allow_blank => false },
+      :items            => [ {
+        :name         => :username,
+        :field_label  => "Username"
+      }, {
+        :name         => :password,
+        :field_label  => "Password",
+        :input_type   => :password
+      }, {
+        :name         => :log_application,
+        :value        => super[:log_application],
+        :hidden       => true
+      }, {
+        :name         => :log_event_source,
+        :value        => super[:log_event_source],
+        :hidden       => true
+      } ]
+    } ]
+  end
+
+  js_configure do |c|
+    c.mixin :main, :actions
+  end
+
+  endpoint :login do |params, this|
+    success   = false
+    user      = User.find_by_username(params[:username])
     if user
       if user.password == params[:password]
         if user.is_enabled
@@ -66,8 +69,7 @@ class LoginWindow < Netzke::Basepack::Window
     else
       Log.warn("User failed to log in: #{params[:username]}")
     end
-
-    { :set_result => success }
+    this.netzke_set_result(success)
   end
 
 end
