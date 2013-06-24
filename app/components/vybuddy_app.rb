cf78cc49db27475a9a8bbd7dd212cc3d94241be5
Ron Jones <ronjones@vyatta4people.org>
2013-06-24 19:47:09 +0300

    Rewrote VybuddyApp component for usage with latest Netzke.

--- app/components/vybuddy_app.rb
+++ app/components/vybuddy_app.rb
@@ -1,42 +1,41 @@
-class VybuddyApp < Netzke::Basepack::BorderLayoutPanel
+class VybuddyApp < Netzke::Base
 
-  js_mixin :init_component
-
-  def configuration
-    super.merge(
-      :name           => :vybuddy_app,
-      :title          => "vyBuddy",
-      :prevent_header => true,
-      :layout         => :border,
-      :body_style     => {"background-color" => "#e4ebef"},
-      :items          => [
-        :toolbox_panel.component(
+  def configure(c)
+    super
+    c.name           = :vybuddy_app,
+    c.title          = "vyBuddy",
+    c.prevent_header = true,
+    c.layout         = :border,
+    c.body_style     = {"background-color" => "#e4ebef"},
+    c.items          = [
+        {
           :name       => :top_panel,
           :region     => :north,
           :class_name => "TopPanel",
           :margin     => "5 5 5 5"
-        ),
-        :vyatta_hosts_grid.component(
+        }, {
           :name       => :vyatta_hosts_grid,
           :region     => :west,
           :class_name => "VyattaHostsGrid",
           :margin     => "0 0 0 5",
           :split      => true
-        ),
-        :display_tasks_tab_panel.component(
+        }, {
+
           :name       => :display_tasks_tab_panel,
           :region     => :center,
           :class_name => "DisplayTasksTabPanel",
           :margin     => "0 5 0 0"
-        ),
-        :bottom_panel.component(
+        }, {
           :name       => :bottom_panel,
           :region     => :south,
           :class_name => "BottomPanel",
           :margin     => "4 4 4 4"
-        )
+        }
       ]
-    )
+  end
+
+  js_configure do |c|
+    c.mixin :main
   end
 
 end
