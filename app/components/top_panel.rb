33f24cf39c14a91eff7e5c6b3fb75e84883cb465
Ron Jones <ronjones@vyatta4people.org>
2013-07-16 13:32:22 +0300

    Rewrote VyBuddyApp component. It is visible now.

--- app/components/top_panel.rb
+++ app/components/top_panel.rb
@@ -1,33 +1,69 @@
-class TopPanel < Netzke::Basepack::Panel
-  action :manage_tasks,         :icon => :table_gear,                 :text => ""
-  action :manage_users,         :icon => :group_gear,                 :text => ""
-  action :manage_ssh_key_pairs, :icon => :server_key,                 :text => ""
-  action :import_export,        :icon => :import_export,              :text => "", :tooltip => "Import/Export"
-  action :view_logs,            :icon => :newspaper_go,               :text => ""
-  action :manage_settings,      :icon => :cog,                        :text => ""
-  action :show_about,           :icon => :information,                :text => ""
-  action :logout,               :icon => :door_out,                   :text => ""
+class TopPanel < Netzke::Base
 
-  js_mixin :actions
+  js_configure do |c|
+    c.mixin :actions
+  end
 
-  def get_bbar
-    if session[:user_is_admin]
-      return [:manage_tasks.action, '->', :manage_users.action, :manage_ssh_key_pairs.action, "-", :import_export.action, "-", :view_logs.action, :manage_settings.action, '-', :show_about.action, '-', :logout.action]
-    else
-      return ['->', :show_about.action, '-', :logout.action]
-    end
+  action :manage_tasks do |a|
+    a.icon = :table_gear
+    a.text = ""
+  end
+
+  action :manage_users do |a|
+    a.icon = :group_gear
+    a.text = ""
+  end
+
+  action :manage_ssh_key_pairs do |a|
+    a.icon = :server_key
+    a.text = ""
+  end
+
+  action :import_export do |a|
+    a.icon    = :import_export
+    a.text    = ""
+    a.tooltip = "Import/Export"
+  end
+
+  action :view_logs do |a|
+    a.icon = :newspaper_go
+    a.text = ""
+  end
+
+  action :manage_settings do |a|
+    a.icon = :cog
+    a.text = ""
+  end
+
+  action :show_about do |a|
+    a.icon = :information
+    a.text = ""
+  end
+
+  action :logout do |a|
+    a.icon = :door_out
+    a.text = ""
   end
 
   def configure(c)
     super
-      :name           => :top_panel,
-      :title          => "Top",
-      :prevent_header => true,
-      :height         => 38,
-      :border         => true,
-      :frame          => true, 
-      :bbar           => get_bbar
-    )
+    c.name           = :top_panel
+    c.title          = "Top (navigation) panel"
+    c.prevent_header = true
+    c.height         = 38
+    c.border         = true
+    c.frame          = true
+    c.bbar           = navigation_bar
+  end
+
+  private
+
+  def navigation_bar
+    if session[:user_is_admin]
+      return [:manage_tasks, '->', :manage_users, :manage_ssh_key_pairs, "-", :import_export, "-", :view_logs, :manage_settings, '-', :show_about, '-', :logout]
+    else
+      return ['->', :show_about, '-', :logout]
+    end
   end
 
 end
--- app/components/vybuddy_app.rb
+++ app/components/vybuddy_app.rb
@@ -10,40 +10,34 @@ class VybuddyApp < Netzke::Basepack::Viewport
     c.name           = :vybuddy_app
     c.title          = "vyBuddy"
     c.prevent_header = true
-    c.layout         = :border
+    #c.layout         = :border
     c.body_style     = {"background-color" => "#e4ebef"}
-    c.items          = [
-     # {
-     #   :netzke_component       => :top_panel,
-     #   :region     => :north,
-     #   :class_name => "TopPanel",
-     #   :margin     => "5 5 5 5"
-     # }, {
-     #   :name       => :vyatta_hosts_grid,
-     #   :region     => :west,
-     #   :class_name => "VyattaHostsGrid",
-     #   :margin     => "0 0 0 5",
-     #   :split      => true
-     # },
-     {
-        netzke_component: :bottom_panel,
-        region: :center#,
-        #js_config: ""
-        #class_name: "DisplayTasksTabPanel1",
-        #margin: "0 5 0 0"
-      }
-    #, {
-    #    :name       => :bottom_panel,
-    #    :region     => :south,
-    #    :class_name => "BottomPanel",
-    #    :margin     => "4 4 4 4"
-    #  }
-    ]
+    c.items          = [ {
+      netzke_component: :top_panel,
+      region:           :north,
+      margin:            "5 5 5 5"
+    }, {
+      netzke_component: :vyatta_hosts_grid,
+      region:           :center,
+      margin:           "0 0 0 5",
+      split:            true
+    }, {
+      netzke_component: :bottom_panel,
+      region:           :south,
+      margin:           "0 5 0 0"
+    } ]
+  end
+
+  component :top_panel do |c|
+    c.klass = TopPanel
+  end
+
+  component :vyatta_hosts_grid do |c|
+    c.klass = VyattaHostsGrid
   end
 
   component :bottom_panel do |c|
     c.klass = BottomPanel
-    c.desc = "DisplayTasksTabPanel"
   end
 
 end
